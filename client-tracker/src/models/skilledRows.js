26ae9a53144d96a90a16f80f87e7c9e34217a4fd
Sai Madan Chennuri <Sai.Madan.Chennuri@ibm.com>
2022-02-20 20:15:41 +0530

    Updated Front End

--- client-tracker/src/models/skilledRows.js
+++ client-tracker/src/models/skilledRows.js
@@ -1,7 +1,8 @@
 export const TABLE_ROWS = [
     {
+        id: '14078',
         technology: 'C#',
-        oDate: '21-06-2021',
+        oDate: '06-21-2021',
         open_positions: 5,
         interviewed: 3,
         shortlist: 2,
@@ -9,8 +10,9 @@ export const TABLE_ROWS = [
         Status: 'open'
     },
     {
+        id: '14079',
         technology: 'JS',
-        oDate: '04-09-2021',
+        oDate: '09-04-2021',
         open_positions: 10,
         interviewed: 4,
         shortlist: 2,
@@ -18,8 +20,9 @@ export const TABLE_ROWS = [
         Status: 'closed'
     },
     {
+        id: '14080',
         technology: 'Java',
-        oDate: '12-10-2021',
+        oDate: '10-12-2021',
         open_positions: 20,
         interviewed: 9,
         shortlist: 3,
@@ -27,6 +30,7 @@ export const TABLE_ROWS = [
         Status: 'open'
     },
     {
+        id: '14081',
         technology: 'Node',
         oDate: '01-01-2021',
         open_positions: 2,
--- client-tracker/src/views/components/skilledTable.js
+++ client-tracker/src/views/components/skilledTable.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import './skilledTable.scss';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { TABLE_COLS } from "../../models/skilledCols";
 import { TABLE_ROWS } from "../../models/skilledRows";
-import './skilledTable.scss';
 
 const SkilledTable = (props) => {
+    const [data, setData] = useState([]);
+    const [timer, setTimer] = useState(null);
+    const [isDataUpdated, setIsDataUpdated] = useState(false);
+
+
+    useEffect(() => {
+        setData(TABLE_ROWS);
+    }, []);
 
     const headerItems = () => {
         let header = [];
@@ -18,19 +28,45 @@ const SkilledTable = (props) => {
         return header;
     }
 
+    const handleChange = (type, row, event) => {
+        if(type === 'oDate') {
+            var dd = String(event.getDate()).padStart(2, '0');
+            var mm = String(event.getMonth() + 1).padStart(2, '0');
+            var yyyy = event.getFullYear();
+
+            let tdata = [...data];             
+            tdata[row][type] = mm + '-' + dd + '-' + yyyy;
+            setData(tdata);
+            setIsDataUpdated(true);
+        } else {
+            let updatedData = event.currentTarget.textContent;
+            clearTimeout(timer);
+            setTimer(
+                setTimeout(() => {   
+                    let tdata = [...data];             
+                    tdata[row][type] = updatedData;
+                    setData(tdata);
+                    setIsDataUpdated(true);
+                }, 3000)
+            );
+        }        
+    }
+
     const getTbody = () => {
         let rows = [];
-        for(let row in TABLE_ROWS) {
-            const tbody = TABLE_ROWS[row];
+        for (let row in data) {
+            const tbody = data[row];
             let item = (
-                <tr>
-                    <td>{tbody.technology}</td>
-                    <td>{tbody.oDate}</td>
-                    <td>{tbody.open_positions}</td>
-                    <td>{tbody.interviewed}</td>
-                    <td>{tbody.shortlist}</td>
-                    <td>{tbody.offer}</td>
-                    <td>{tbody.Status}</td>
+                <tr key={tbody.id}>
+                    <td contentEditable="true" onInput={(e) => handleChange("technology", row, e)}>{tbody.technology}</td>
+                    <td>
+                        <DatePicker selected={new Date(tbody.oDate)} onChange={(e) => handleChange("oDate", row, e)} />
+                    </td>
+                    <td contentEditable="true" onInput={(e) => handleChange("open_positions", row, e)}>{tbody.open_positions}</td>
+                    <td contentEditable="true" onInput={(e) => handleChange("interviewed", row, e)}>{tbody.interviewed}</td>
+                    <td contentEditable="true" onInput={(e) => handleChange("shortlist", row, e)}>{tbody.shortlist}</td>
+                    <td contentEditable="true" onInput={(e) => handleChange("offer", row, e)}>{tbody.offer}</td>
+                    <td contentEditable="true" onInput={(e) => handleChange("Status", row, e)}>{tbody.Status}</td>
                 </tr>
             );
             rows.push(item);
@@ -48,6 +84,12 @@ const SkilledTable = (props) => {
                     {getTbody()}
                 </tbody>
             </table>
+            {
+                isDataUpdated 
+                &&
+                <button className='updateBtn'>Update</button>
+            }
+            
         </div>
     )
 };
